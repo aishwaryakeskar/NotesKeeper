db54d9b5834f4862464d70c82746eed86c1ad2a6
Aishwarya Keskar <aishwaryakeskar98@gmail.com>
2020-05-11 18:32:22 +0530

    Add note by user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateNoteArea";
 
 function App() {
+
+    const [notes, setNotes]  =useState([]);
+
+    function addNote(note){
+        setNotes(prevNotes => {
+            return [...prevNotes, note];
+        });
+    };
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea 
+          onAdd={addNote}
+      />
+      {notes.map(note => 
+      <Note title={note.title} content={note.content}/>
+      )}
       <Footer />
     </div>
   );
